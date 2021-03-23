8baf372a59e1d312b36497cf687d861bd9e0807c
Andrea <amenichelli1991@gmail.com>
2021-03-23 08:44:19 +0100

    done: commandLineUI, maybe use a listener for calculations

--- src/com/thenewjava/calc/commandLineUI.java
+++ src/com/thenewjava/calc/commandLineUI.java
@@ -2,6 +2,17 @@ package com.thenewjava.calc;
 
 public class commandLineUI {
     public void createUI(){
+        System.out.println("___________________________________________________________________________________________________________________________\n" +
+            "|\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t  |\n" +
+            "|\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t  |\t|\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t  |\n" +
+            "|\t\t\t\t\tBenvenuto nella calcolatrice di Andrea\t\t\t\t\t\t  |\n" +
+            "|\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t  |\n" +
+            "|\t\t\tInserisci il primo operando, l'operazione, il secondo operando e premi invio\t\t\t  |\n" +
+            "|     Le operazioni possibili sono somma, sottrarre il secondo dal primo,Moltiplicarli, Dividerli, Restituire il resto    |\n" +
+            "|     della divisione, Restituire il numero più grande (il massimo), Restituire il numero più piccolo (il minimo),        |\n" +
+            "|    \t\t\t\t\t Restituire la media dei due numeri. Digita exit per uscire                       | |\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t  |\n" +
+            "|\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t  |\t|\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t  |\n" +
+            "|_________________________________________________________________________________________________________________________|");
         while(true){
 
         }
